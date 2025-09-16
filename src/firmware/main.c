39fc445769abd32bc9a07a5b4604633a74365e75
Florent Kermarrec <florent@enjoy-digital.fr>
2025-09-16 16:16:17 +0200

    firmware/main: Replace test firmware with initial adaptation of original firmware (compiles and generate message, but VCTCXO not yet in place/tested).

--- src/firmware/main.c
+++ src/firmware/main.c
@@ -1,117 +1,43 @@
+/*--------------------------------------------------------------------------
+-- FILE        :	main.c
+-- DESCRIPTION :	LimePSB RPCM GPSDO CPU Firmware
+-- DATE        :
+-- AUTHOR(s)   :	Lime Microsystems
+-- REVISIONS   :
+--------------------------------------------------------------------------*/
+
 #include <stdio.h>
+#include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 #include <irq.h>
 #include <libbase/uart.h>
 #include <libbase/console.h>
+
+#include <generated/soc.h>
+#include <generated/mem.h>
 #include <generated/csr.h>
+
+#include "vctcxo_tamer.h"
 
 /*-----------------------------------------------------------------------*/
 /* Constants                                                             */
 /*-----------------------------------------------------------------------*/
 
+#define DEBUG_PRINT
+
 /*-----------------------------------------------------------------------*/
 /* Global Variables                                                      */
 /*-----------------------------------------------------------------------*/
 
+struct vctcxo_tamer_pkt_buf vctcxo_tamer_pkt;
+uint16_t dac_val = 30714;
+
 /*-----------------------------------------------------------------------*/
 /* Helpers                                                               */
 /*-----------------------------------------------------------------------*/
-
-void busy_wait(unsigned int ms)
-{
-	timer0_en_write(0);
-	timer0_reload_write(0);
-	timer0_load_write(CONFIG_CLOCK_FREQUENCY/1000*ms);
-	timer0_en_write(1);
-	timer0_update_value_write(1);
-	while(timer0_value_read()) timer0_update_value_write(1);
-}
-
-/*-----------------------------------------------------------------------*/
-/* UART                                                                  */
-/*-----------------------------------------------------------------------*/
-
-static char *readstr(void)
-{
-	char c[2];
-	static char s[64];
-	static int ptr = 0;
-
-	if(readchar_nonblock()) {
-		c[0] = getchar();
-		c[1] = 0;
-		switch(c[0]) {
-			case 0x7f:
-			case 0x08:
-				if(ptr > 0) {
-					ptr--;
-					fputs("\x08 \x08", stdout);
-				}
-				break;
-			case 0x07:
-				break;
-			case '\r':
-			case '\n':
-				s[ptr] = 0x00;
-				fputs("\n", stdout);
-				ptr = 0;
-				return s;
-			default:
-				if(ptr >= (sizeof(s) - 1))
-					break;
-				fputs(c, stdout);
-				s[ptr] = c[0];
-				ptr++;
-				break;
-		}
-	}
-
-	return NULL;
-}
-
-static char *get_token(char **str)
-{
-	char *c, *d;
-
-	c = (char *)strchr(*str, ' ');
-	if(c == NULL) {
-		d = *str;
-		*str = *str+strlen(*str);
-		return d;
-	}
-	*c = 0;
-	d = *str;
-	*str = c+1;
-	return d;
-}
-
-static void prompt(void)
-{
-	printf("\e[92;1mppsdo\e[0m> ");
-}
-
-/*-----------------------------------------------------------------------*/
-/* Help                                                                  */
-/*-----------------------------------------------------------------------*/
-
-static void help(void)
-{
-	puts("\nPPSDO firmware built "__DATE__" "__TIME__"\n");
-	puts("Available commands:");
-	puts("help               - Show this command");
-	puts("reboot             - Reboot CPU");
-}
-
-/*-----------------------------------------------------------------------*/
-/* Commands                                                              */
-/*-----------------------------------------------------------------------*/
-
-static void reboot_cmd(void)
-{
-	ctrl_reset_write(1);
-}
 
 /**
  * @brief Controls the TCXO DAC via SPI by sending configuration data.
@@ -145,52 +71,236 @@ static void Control_TCXO_DAC(uint8_t pd, uint16_t data) {
 	while(spi_status_read() != SPI_DONE);
 }
 
-static void dac_test(void)
-{
-	while (1) {
-		Control_TCXO_DAC(0, 65535);
-		busy_wait(1000);
-		Control_TCXO_DAC(0, 0);
-		busy_wait(1000);
-	}
-}
-
-
 /*-----------------------------------------------------------------------*/
-/* Console service / Main                                                */
+/* Main                                                                  */
 /*-----------------------------------------------------------------------*/
-
-static void console_service(void)
-{
-	char *str;
-	char *token;
-
-	str = readstr();
-	if(str == NULL) return;
-	token = get_token(&str);
-	if(strcmp(token, "help") == 0)
-		help();
-	else if(strcmp(token, "reboot") == 0)
-		reboot_cmd();
-	else if(strcmp(token, "i2c_test") == 0)
-	prompt();
-}
-
 int main(void)
 {
 #ifdef CONFIG_CPU_HAS_INTERRUPT
 	irq_setmask(0);
 	irq_setie(1);
 #endif
+
+#ifdef DEBUG_PRINT
 	uart_init();
+	puts("\nLimePSB-RPCM GPSDO CPU Firmware\n");
+#endif
+	// Trim DAC constants
+  	const uint16_t trimdac_min = 0x0000; // Decimal value = 0
+  	const uint16_t trimdac_max = 0xFFFF; // Decimal value = 65535
 
-	help();
-	prompt();
-	dac_test();
+    // Trim DAC calibration line
+    line_t trimdac_cal_line;
 
-	while(1) {
-		console_service();
-	}
+    // VCTCXO Tune State machine
+    state_t tune_state = COARSE_TUNE_MIN;
 
-	return 0;
+    // Set the known/default values of the trim DAC cal line
+    trimdac_cal_line.point[0].x = 0;
+    trimdac_cal_line.point[0].y = trimdac_min;
+    trimdac_cal_line.point[1].x = 0;
+    trimdac_cal_line.point[1].y = trimdac_max;
+    trimdac_cal_line.slope = 0;
+    trimdac_cal_line.y_intercept = 0;
+    vctcxo_tamer_pkt.ready = false;
+
+    uint8_t vctcxo_tamer_en=0,	vctcxo_tamer_en_old = 0;
+
+    /* Set Default VCTCXO DAC value */
+    vctcxo_trim_dac_write(0x08, 0x77FA);
+    Control_TCXO_DAC(0x0, 0x77FA);  //Set DAC in normal operation mode, set new val
+
+//#ifdef DEBUG_PRINT
+//  printf("VT_DAC_TUNNED_VAL_ADDR0=0x%02x\n", vctcxo_tamer_read(VT_DAC_TUNNED_VAL_ADDR0));
+//  printf("VT_DAC_TUNNED_VAL_ADDR1=0x%02x\n", vctcxo_tamer_read(VT_DAC_TUNNED_VAL_ADDR1));
+//#endif
+
+ while (1)
+  {
+    //Get vctcxo tamer enable bit status
+    vctcxo_tamer_en_old = vctcxo_tamer_en;
+#if 0
+    vctcxo_tamer_en = (uint8_t)(GPIO_INPUT & 0x0001);
+#else
+    vctcxo_tamer_en = 1; /* FIXME: Replace with CSR and input */
+#endif
+
+    // Enable or disable VCTCXO tamer module depending on enable signal
+    if (vctcxo_tamer_en_old != vctcxo_tamer_en){
+    	if (vctcxo_tamer_en == 0x01){  //Enable
+    		vctcxo_tamer_init();
+        tune_state = COARSE_TUNE_MIN;
+    		vctcxo_tamer_pkt.ready = true;
+    	}
+    	else {  //Disable
+    		vctcxo_tamer_dis();
+    		tune_state = COARSE_TUNE_MIN;
+    		vctcxo_tamer_pkt.ready = false;
+    	}
+    }
+
+    /* VCTCXO Calibration FSM. */
+    if (vctcxo_tamer_pkt.ready)
+    {
+      vctcxo_tamer_pkt.ready = false;
+
+      switch (tune_state)
+      {
+
+      case COARSE_TUNE_MIN:
+
+        /* Tune to the minimum DAC value */
+        vctcxo_trim_dac_write(0x08, trimdac_min);
+        dac_val = (uint16_t)trimdac_min;
+        Control_TCXO_DAC(0x0, dac_val);  //Set DAC in normal operation mode, set new val
+
+        /* State to enter upon the next interrupt */
+        tune_state = COARSE_TUNE_MAX;
+        #ifdef DEBUG_PRINT
+          puts("\nCOARSE_TUNE_MIN\n");
+          // printf("COARSE_TUNE_MIN: \r\n\t");
+          // printf("DAC value: ");
+          // printf("%u;\t", (unsigned int)dac_val);
+        #endif
+
+        break;
+
+      case COARSE_TUNE_MAX:
+        /* We have the error from the minimum DAC setting, store it
+         * as the 'x' coordinate for the first point */
+        trimdac_cal_line.point[0].x = vctcxo_tamer_pkt.pps_1s_error;
+
+        // printf("1s_error: ");
+        // printf("%i;\r\n", (int)vctcxo_tamer_pkt.pps_1s_error);
+
+        /* Tune to the maximum DAC value */
+        vctcxo_trim_dac_write(0x08, trimdac_max);
+        dac_val = (uint16_t)trimdac_max;
+        Control_TCXO_DAC(0x0, dac_val);  //Set DAC in normal operation mode, set new val
+
+        /* State to enter upon the next interrupt */
+        tune_state = COARSE_TUNE_DONE;
+        #ifdef DEBUG_PRINT
+          puts("\nCOARSE_TUNE_MAX\n");
+        #endif
+
+        break;
+
+      case COARSE_TUNE_DONE:
+        /* Write status to to state register*/
+        vctcxo_tamer_write(VT_STATE_ADDR, 0x01);
+
+        /* We have the error from the maximum DAC setting, store it
+         * as the 'x' coordinate for the second point */
+        trimdac_cal_line.point[1].x = vctcxo_tamer_pkt.pps_1s_error;
+
+        // printf("1s_error: ");
+        // printf("%i;\r\n", (int)vctcxo_tamer_pkt.pps_1s_error);
+
+        /* We now have two points, so we can calculate the equation
+         * for a line plotted with DAC counts on the Y axis and
+         * error on the X axis. We want a PPM of zero, which ideally
+         * corresponds to the y-intercept of the line. */
+
+        trimdac_cal_line.slope = ((float)(trimdac_cal_line.point[1].y - trimdac_cal_line.point[0].y) / (float)(trimdac_cal_line.point[1].x - trimdac_cal_line.point[0].x));
+
+        trimdac_cal_line.y_intercept = (trimdac_cal_line.point[0].y -
+                                        (uint16_t)(lroundf(trimdac_cal_line.slope * (float)trimdac_cal_line.point[0].x)));
+
+        /* Set the trim DAC count to the y-intercept */
+        vctcxo_trim_dac_write(0x08, trimdac_cal_line.y_intercept);
+        dac_val = (uint16_t)trimdac_cal_line.y_intercept;
+        Control_TCXO_DAC(0x0, dac_val);  //Set DAC in normal operation mode, set new val
+
+        /* State to enter upon the next interrupt */
+        tune_state = FINE_TUNE;
+        #ifdef DEBUG_PRINT
+          puts("\nCOARSE_TUNE_DONE\n");
+        #endif
+
+        break;
+
+      case FINE_TUNE:
+        #ifdef DEBUG_PRINT
+          puts("\nFINE_TUNE\n");
+        #endif
+
+        /* We should be extremely close to a perfectly tuned
+         * VCTCXO, but some minor adjustments need to be made */
+
+        /* Check the magnitude of the errors starting with the
+         * one second count. If an error is greater than the maximum
+         * tolerated error, adjust the trim DAC by the error (Hz)
+         * multiplied by the slope (in counts/Hz) and scale the
+         * result by the precision interval (e.g. 1s, 10s, 100s). */
+
+        if (vctcxo_tamer_pkt.pps_1s_error_flag)
+        {
+          vctcxo_trim_dac_value = (vctcxo_trim_dac_value -
+                                   (uint16_t)(lroundf((float)vctcxo_tamer_pkt.pps_1s_error * trimdac_cal_line.slope) / 1));
+          // Write tuned val to VCTCXO_tamer MM registers
+          vctcxo_trim_dac_write(0x08, vctcxo_trim_dac_value);
+          // Change DAC value
+          dac_val = (uint16_t)vctcxo_trim_dac_value;
+          Control_TCXO_DAC(0x0, dac_val);  //Set DAC in normal operation mode, set new val
+          // printf("001;");
+          // printf("%u;", (unsigned int)dac_val);
+          // printf("%i;\r\n", (int) vctcxo_tamer_pkt.pps_1s_error);
+        }
+        else if (vctcxo_tamer_pkt.pps_10s_error_flag)
+        {
+          vctcxo_trim_dac_value = (vctcxo_trim_dac_value -
+                                   (uint16_t)(lroundf((float)vctcxo_tamer_pkt.pps_10s_error * trimdac_cal_line.slope) / 10));
+          // Write tuned val to VCTCXO_tamer MM registers
+          vctcxo_trim_dac_write(0x08, vctcxo_trim_dac_value);
+          // Change DAC value
+          dac_val = (uint16_t)vctcxo_trim_dac_value;
+          Control_TCXO_DAC(0x0, dac_val);  //Set DAC in normal operation mode, set new val
+          // printf("010;");
+          // printf("%u;", (unsigned int)dac_val);
+          // printf("%i;\r\n", (int) vctcxo_tamer_pkt.pps_10s_error);
+        }
+        else if (vctcxo_tamer_pkt.pps_100s_error_flag)
+        {
+          vctcxo_trim_dac_value = (vctcxo_trim_dac_value -
+                                   (uint16_t)(lroundf((float)vctcxo_tamer_pkt.pps_100s_error * trimdac_cal_line.slope) / 100));
+          // Write tuned val to VCTCXO_tamer MM registers
+          vctcxo_trim_dac_write(0x08, vctcxo_trim_dac_value);
+          // Change DAC value
+          dac_val = (uint16_t)vctcxo_trim_dac_value;
+          Control_TCXO_DAC(0x0, dac_val);  //Set DAC in normal operation mode, set new val
+          // printf("100;");
+          // printf("%u;", (unsigned int)dac_val);
+          // printf("%i;\r\n", (int) vctcxo_tamer_pkt.pps_100s_error);
+        }
+
+        break;
+
+      default:
+        break;
+
+      } /* switch */
+
+      /* Take PPS counters out of reset */
+      vctcxo_tamer_reset_counters(false);
+
+      /* Enable interrupts */
+      vctcxo_tamer_enable_isr(true);
+
+    } /* VCTCXO Tamer interrupt */
+  }
+
+  return 0;
 }
+
+#if 0
+// handler functions for the external interrupt channels:
+// - must not have parameters nor a return value
+// - should not use the interrupt attribute, as they can be normal functions called by the actual interrupt handler
+
+void ext_irq_ch0_handler(void)
+{
+  vctcxo_tamer_isr(&vctcxo_tamer_pkt);
+}
+
+#endif
