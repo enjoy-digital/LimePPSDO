96f7146a4fc7dba1ebf3d2fd0fe0d273729a0983
Florent Kermarrec <florent@enjoy-digital.fr>
2025-09-18 14:58:46 +0200

    firmware: Cleanup vctcxo_tamer .c/.h files with similar coding style than main.c.

--- src/firmware/vctcxo_tamer.c
+++ src/firmware/vctcxo_tamer.c
@@ -1,3 +1,11 @@
+/*--------------------------------------------------------------------------
+-- FILE        : vctcxo_tamer.c
+-- DESCRIPTION : VCTCXO Tamer file.
+-- DATE        :
+-- AUTHOR(s)   : Lime Microsystems.
+-- REVISIONS   :
+--------------------------------------------------------------------------*/
+
 #include <stdbool.h>
 #include <stdint.h>
 
@@ -5,24 +13,35 @@
 
 #include "vctcxo_tamer.h"
 
-/* Define a cached version of the VCTCXO tamer control register */
-uint8_t vctcxo_tamer_ctrl_reg = 0x00;
+/*-----------------------------------------------------------------------*/
+/* Global Variables                                                      */
+/*-----------------------------------------------------------------------*/
 
-/* Define a global variable containing the current VCTCXO DAC setting.
- * This is a 'cached' value of what is written to the DAC and is used
- * by the VCTCXO calibration algorithm to avoid constant read requests
- * going out to the DAC. Initial power-up state of the DAC is mid-scale.
+/* Cached version of the VCTCXO Tamer control register. */
+uint8_t vctcxo_tamer_ctrl_reg;
+
+/* Global variable containing the current VCTCXO DAC setting. This is a 'cached'
+   value of what is written to the DAC and is used by the VCTCXO calibration
+   algorithm to avoid constant read requests going out to the DAC. Initial
+   power-up state of the DAC is mid-scale.
  */
-uint16_t vctcxo_trim_dac_value = 0x77FA;
+uint16_t vctcxo_trim_dac_value;
 
+/*-----------------------------------------------------------------------*/
+/* Functions                                                             */
+/*-----------------------------------------------------------------------*/
+
+/* Reads a byte from VCTCXO Tamer register. */
 uint8_t vctcxo_tamer_read(uint8_t addr) {
     return *(volatile uint8_t *)(VCTCXO_TAMER_BASE + 4*addr);
 }
 
+/* Writes a byte to VCTCXO Tamer register. */
 void vctcxo_tamer_write(uint8_t addr, uint8_t data) {
     *(volatile uint8_t *)(VCTCXO_TAMER_BASE + 4*addr) = data;
 }
 
+/* Resets or releases the PPS counters. */
 void vctcxo_tamer_reset_counters(bool reset) {
     if( reset ) {
         vctcxo_tamer_ctrl_reg |= VT_CTRL_RESET;
@@ -34,6 +53,7 @@ void vctcxo_tamer_reset_counters(bool reset) {
     return;
 }
 
+/* Enables or disables the VCTCXO Tamer interrupt. */
 void vctcxo_tamer_enable_isr(bool enable) {
     if( enable ) {
         vctcxo_tamer_ctrl_reg |= VT_CTRL_IRQ_EN;
@@ -45,11 +65,13 @@ void vctcxo_tamer_enable_isr(bool enable) {
     return;
 }
 
+/* Clears the VCTCXO Tamer interrupt. */
 void vctcxo_tamer_clear_isr(void) {
     vctcxo_tamer_write(VT_CTRL_ADDR, vctcxo_tamer_ctrl_reg | VT_CTRL_IRQ_CLR);
     return;
 }
 
+/* Sets the tuning mode for the VCTCXO Tamer. */
 void vctcxo_tamer_set_tune_mode(vctcxo_tamer_mode mode) {
 
     switch (mode) {
@@ -64,15 +86,15 @@ void vctcxo_tamer_set_tune_mode(vctcxo_tamer_mode mode) {
             return;
     }
 
-    /* Set tuning mode */
+    /* Set tuning mode. */
     vctcxo_tamer_ctrl_reg &= ~VT_CTRL_TUNE_MODE;
     vctcxo_tamer_ctrl_reg |= (((uint8_t) mode) << 6);
     vctcxo_tamer_write(VT_CTRL_ADDR, vctcxo_tamer_ctrl_reg);
 
-    /* Reset the counters */
+    /* Reset the counters. */
     vctcxo_tamer_reset_counters( true );
 
-    /* Take counters out of reset if tuning mode is not DISABLED */
+    /* Take counters out of reset if tuning mode is not DISABLED. */
     if( mode != 0x00 ) {
         vctcxo_tamer_reset_counters( false );
     }
@@ -84,96 +106,84 @@ void vctcxo_tamer_set_tune_mode(vctcxo_tamer_mode mode) {
             break;
 
         default:
-            /* Leave ISR disabled otherwise */
+            /* Leave ISR disabled otherwise. */
             break;
     }
 
     return;
 }
 
+/* Reads a 32-bit count from VCTCXO Tamer registers. */
 int32_t vctcxo_tamer_read_count(uint8_t addr) {
-    //uint32_t base = AVALON_MM_EXTERNAL_0_BASE;
-    //TODO: ???
-    //uint32_t base = XPAR_AVMM_M0_BASEADDR;
     uint8_t offset = addr;
     int32_t value = 0;
-
-    //TODO: replace with neo430 wishbone functions
-    //value  = Xil_In8(base + offset++);
-    //value |= ((int32_t) Xil_In8(base + offset++)) << 8;
-    //value |= ((int32_t) Xil_In8(base + offset++)) << 16;
-    //value |= ((int32_t) Xil_In8(base + offset++)) << 24;
 
     value  = (int32_t)vctcxo_tamer_read((uint32_t) offset++);
     value |= (int32_t)(vctcxo_tamer_read((uint32_t) offset++)) << 8;
     value |= (int32_t)(vctcxo_tamer_read((uint32_t) offset++)) << 16;
     value |= (int32_t)(vctcxo_tamer_read((uint32_t) offset++)) << 24;
-    
+
     return value;
 }
 
+/* Writes the trim DAC value to VCTCXO Tamer registers. */
 void vctcxo_trim_dac_write(uint8_t cmd, uint16_t val)
 {
-	uint8_t tuned_val_lsb;
-	uint8_t tuned_val_msb;
+    uint8_t tuned_val_lsb;
+    uint8_t tuned_val_msb;
 
-	tuned_val_lsb = (uint8_t) (val & 0x00FF);
-	tuned_val_msb = (uint8_t) ((val & 0xFF00) >> 8);
+    tuned_val_lsb = (uint8_t) (val & 0x00FF);
+    tuned_val_msb = (uint8_t) ((val & 0xFF00) >> 8);
 
-    //write tuned val to VCTCXO_tamer MM registers
+    /* Write tuned val to VCTCXO Tamer registers. */
     vctcxo_tamer_write(VT_DAC_TUNNED_VAL_ADDR0, tuned_val_lsb);
     vctcxo_tamer_write(VT_DAC_TUNNED_VAL_ADDR1, tuned_val_msb);
-    
-
-    
-
 }
 
-
+/* VCTCXO Tamer ISR handler. */
 void vctcxo_tamer_isr(void *context) {
     struct vctcxo_tamer_pkt_buf *pkt = (struct vctcxo_tamer_pkt_buf *)context;
     uint8_t error_status = 0x00;
 
-    /* Disable interrupts */
+    /* Disable interrupts. */
     vctcxo_tamer_enable_isr( false );
 
-    /* Reset (stop) the counters */
+    /* Reset (stop) the PPS counters. */
     vctcxo_tamer_reset_counters( true );
 
-    /* Read the current count values */
+    /* Read the current count values. */
     pkt->pps_1s_error   = vctcxo_tamer_read_count(VT_ERR_1S_ADDR);
     pkt->pps_10s_error  = vctcxo_tamer_read_count(VT_ERR_10S_ADDR);
     pkt->pps_100s_error = vctcxo_tamer_read_count(VT_ERR_100S_ADDR);
 
-    /* Read the error status register */
+    /* Read the error status register. */
     error_status = vctcxo_tamer_read(VT_STAT_ADDR);
 
-    /* Set the appropriate flags in the packet buffer */
+    /* Set the appropriate flags in the packet buffer. */
     pkt->pps_1s_error_flag   = (error_status & VT_STAT_ERR_1S)   ? true : false;
     pkt->pps_10s_error_flag  = (error_status & VT_STAT_ERR_10S)  ? true : false;
     pkt->pps_100s_error_flag = (error_status & VT_STAT_ERR_100S) ? true : false;
 
-    /* Clear interrupt */
+    /* Clear interrupt. */
     vctcxo_tamer_clear_isr();
 
-    /* Tell the main loop that there is a request pending */
+    /* Tell the main loop that there is a request pending. */
     pkt->ready = true;
 
     return;
 }
 
-
+/* Initializes the VCTCXO Tamer. */
 void vctcxo_tamer_init(void){
     /* Default VCTCXO Tamer and its interrupts to be disabled. */
-	vctcxo_tamer_write(VT_STATE_ADDR, 0x00);
-	/* Write status to to state register*/
+    vctcxo_tamer_write(VT_STATE_ADDR, 0x00);
     vctcxo_tamer_set_tune_mode(VCTCXO_TAMER_1_PPS);
 }
 
+/* Disables the VCTCXO Tamer. */
 void vctcxo_tamer_dis(void){
     /* Default VCTCXO Tamer and its interrupts to be disabled. */
     vctcxo_tamer_set_tune_mode(VCTCXO_TAMER_DISABLED);
 
-	/* Write status to to state register*/
-	vctcxo_tamer_write(VT_STATE_ADDR, 0x00);
+    vctcxo_tamer_write(VT_STATE_ADDR, 0x00);
 }
--- src/firmware/vctcxo_tamer.h
+++ src/firmware/vctcxo_tamer.h
@@ -1,58 +1,65 @@
-/*
- * vctcxo_tamer.h
- *
- *  Created on: Feb 19, 2018
- *      Author: Vytautas
- */
+/*--------------------------------------------------------------------------
+-- FILE        : vctcxo_tamer.h
+-- DESCRIPTION : VCTCXO Tamer header file.
+-- DATE        :
+-- AUTHOR(s)   : Lime Microsystems.
+-- REVISIONS   :
+--------------------------------------------------------------------------*/
 
 #ifndef VCTCXO_TAMER_H_
 #define VCTCXO_TAMER_H_
 
-/* VCTCXO tamer register offsets */
-#   define VT_CTRL_ADDR      		(0x00)
-#   define VT_STAT_ADDR      		(0x01)
-#   define VT_ERR_1S_ADDR    		(0x04)
-#   define VT_ERR_10S_ADDR   		(0x0C)
-#   define VT_ERR_100S_ADDR  		(0x14)
-# 	define VT_STATE_ADDR     		(0x1C)
-# 	define VT_DAC_TUNNED_VAL_ADDR0 	(0x20)
-# 	define VT_DAC_TUNNED_VAL_ADDR1 	(0x21)
+/*-----------------------------------------------------------------------*/
+/* Register Offsets                                                      */
+/*-----------------------------------------------------------------------*/
 
-/* VCTCXO tamer control/status bits */
-#   define VT_CTRL_RESET     		(0x01)
-#   define VT_CTRL_IRQ_EN    		(1<<4)
-#   define VT_CTRL_IRQ_CLR   		(1<<5)
-#   define VT_CTRL_TUNE_MODE 		(0xC0)
+#define VT_CTRL_ADDR             (0x00)
+#define VT_STAT_ADDR             (0x01)
+#define VT_ERR_1S_ADDR           (0x04)
+#define VT_ERR_10S_ADDR          (0x0C)
+#define VT_ERR_100S_ADDR         (0x14)
+#define VT_STATE_ADDR            (0x1C)
+#define VT_DAC_TUNNED_VAL_ADDR0  (0x20)
+#define VT_DAC_TUNNED_VAL_ADDR1  (0x21)
 
-#   define VT_STAT_ERR_1S    		(0x01)
-#   define VT_STAT_ERR_10S   		(1<<1)
-#   define VT_STAT_ERR_100S  		(1<<2)
+/*-----------------------------------------------------------------------*/
+/* Control/Status Bits                                                   */
+/*-----------------------------------------------------------------------*/
 
+#define VT_CTRL_RESET            (0x01)
+#define VT_CTRL_IRQ_EN           (1<<4)
+#define VT_CTRL_IRQ_CLR          (1<<5)
+#define VT_CTRL_TUNE_MODE        (0xC0)
 
-/* Define a cached version of the VCTCXO tamer control register */
+#define VT_STAT_ERR_1S           (0x01)
+#define VT_STAT_ERR_10S          (1<<1)
+#define VT_STAT_ERR_100S         (1<<2)
+
+/* Cached version of the VCTCXO Tamer control register. */
 extern uint8_t vctcxo_tamer_ctrl_reg;
 
-/* Define a global variable containing the current VCTCXO DAC setting.
- * This is a 'cached' value of what is written to the DAC and is used
- * by the VCTCXO calibration algorithm to avoid constant read requests
- * going out to the DAC. Initial power-up state of the DAC is mid-scale.
+/* Global variable containing the current VCTCXO DAC setting. This is a 'cached'
+   value of what is written to the DAC and is used by the VCTCXO calibration
+   algorithm to avoid constant read requests going out to the DAC. Initial
+   power-up state of the DAC is mid-scale.
  */
 extern uint16_t vctcxo_trim_dac_value;
 
-/* A structure that represents a point on a line. Used for calibrating
- * the VCTCXO */
+/* Structure that represents a point on a line. Used for calibrating the
+   VCTCXO.
+ */
 typedef struct point {
-    int32_t  x; // Error counts
-    uint16_t y; // DAC count
+    int32_t  x; /* Error counts. */
+    uint16_t y; /* DAC count. */
 } point_t;
 
 typedef struct line {
     point_t  point[2];
     float  slope;
-    uint16_t y_intercept; // in DAC counts
+    uint16_t y_intercept; /* In DAC counts. */
 } line_t;
 
-/* State machine for VCTCXO tuning */
+/* State machine for VCTCXO tuning. */
 typedef enum state {
     COARSE_TUNE_MIN,
     COARSE_TUNE_MAX,
@@ -62,16 +69,16 @@ typedef enum state {
 } state_t;
 
 typedef enum {
-    /** Denotes an invalid selection or state */
+    /* Denotes an invalid selection or state. */
     VCTCXO_TAMER_INVALID = -1,
 
-    /** Do not attempt to tame the VCTCXO with an input source. */
+    /* Do not attempt to tame the VCTCXO with an input source. */
     VCTCXO_TAMER_DISABLED = 0,
 
-    /** Use a 1 pps input source to tame the VCTCXO. */
+    /* Use a 1 pps input source to tame the VCTCXO. */
     VCTCXO_TAMER_1_PPS = 1,
 
-    /** Use a 10 MHz input source to tame the VCTCXO. */
+    /* Use a 10 MHz input source to tame the VCTCXO. */
     VCTCXO_TAMER_10_MHZ = 2
 } vctcxo_tamer_mode;
 
@@ -85,23 +92,27 @@ struct vctcxo_tamer_pkt_buf {
     volatile bool    pps_100s_error_flag;
 };
 
-uint8_t vctcxo_tamer_read(uint8_t addr) ;
+/*-----------------------------------------------------------------------*/
+/* Function Prototypes                                                   */
+/*-----------------------------------------------------------------------*/
 
-void vctcxo_tamer_write(uint8_t addr, uint8_t data) ;
+uint8_t vctcxo_tamer_read(uint8_t addr);
 
-void vctcxo_tamer_reset_counters(bool reset) ;
+void vctcxo_tamer_write(uint8_t addr, uint8_t data);
 
-void vctcxo_tamer_enable_isr(bool enable) ;
+void vctcxo_tamer_reset_counters(bool reset);
+
+void vctcxo_tamer_enable_isr(bool enable);
 
 void vctcxo_tamer_clear_isr(void);
 
-void vctcxo_tamer_set_tune_mode(vctcxo_tamer_mode mode) ;
+void vctcxo_tamer_set_tune_mode(vctcxo_tamer_mode mode);
 
-int32_t vctcxo_tamer_read_count(uint8_t addr) ;
+int32_t vctcxo_tamer_read_count(uint8_t addr);
 
 void vctcxo_trim_dac_write(uint8_t cmd, uint16_t val);
 
-void vctcxo_tamer_isr(void *context) ;
+void vctcxo_tamer_isr(void *context);
 
 void vctcxo_tamer_init(void);
 
